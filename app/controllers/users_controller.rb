b7211f4399cd8e47a696f821f4daf42b74f8acd1
ken025 <kennethmanuel250@gmail.com>
2020-10-29 23:47:51 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,35 @@
 class UsersController < ApplicationController
 
-    def create
+      def create
         @user = User.new(user_params)
         if @user && @user.save
           session[:user_id] = @user.id
+          redirect_to user_path(@user)
         else 
           redirect_to '/signup'
         end
       end
      
     def new
+      @user = User.new
     end 
 
-    def mood
-    end 
-    
+    def show
+      @user = User.find_by_id(params[:id])
+    end
+
     private
-     
-    def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
 
-end 
+      def user_params
+        params.require(:user).permit(
+          :name,
+          :height,
+          :nausea,
+          :tickets,
+          :admin,
+          :password,
+          :happiness
+        )
+      end
+ 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-
-  root 'application#hello'
   resources :users
   resources :rides
   resources :attractions
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
+  root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
